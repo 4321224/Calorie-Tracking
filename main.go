18bfdf9ed4ae92ba2904769e204b38cad9db55b7
tanjung anggraini agustin <108222692+4321224@users.noreply.github.com>
2023-03-22 03:19:03 +0000

    save before sleep

--- main.go
+++ main.go
@@ -1,2 +1,23 @@
 package main
 
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+	"Calorie-Tracking/routes"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(cors.Default())	
+
+	router.POST("/entries/create", routes.AddEntry)
+	router.GET("/entries")
+}
